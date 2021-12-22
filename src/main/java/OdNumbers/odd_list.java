3bfe08a7aa4494f5eb6ec97f62e9df393324620d
Oleksandr <zahirolek@gmail.com>
2021-12-22 18:38:18 +0200

    38.	write code to solve the task List<Integer> numbers = Arrays.asList(2,1,3,5,2,3,4,4,5,5,7,8,9,0,9,8,7,6,3); List<Integer> oddNumbers;

---
+++ src/main/java/OdNumbers/odd_list.java
@@ -0,0 +1,20 @@
+package OdNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class odd_list {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(2,1,3,5,2,3,4,4,5,5,7,8,9,0,9,8,7,6,3);
+        System.out.print("The even numbers in the list: ");
+        numbers.stream().filter( i -> i %2 == 0 ).distinct().forEach(i -> System.out.print(" " + i));
+
+        System.out.print("\n The odd numbers in the list: ");
+        numbers.stream().filter( i -> i %2 != 0 ).distinct().forEach(i -> System.out.print(" " + i));
+        //List<Integer> oddNumbers;
+
+
+        
+
+    }
+}
