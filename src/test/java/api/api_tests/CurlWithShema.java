2edc9d23c693207ff08ce67e523b840c4b86f6e9
Oleksandr <zahirolek@gmail.com>
2021-12-23 21:20:06 +0200

    Write a code for this request and test for the response. curl -X POST add

--- src/test/java/api/api_tests/CurlWithShema.java
+++ src/test/java/api/api_tests/CurlWithShema.java
@@ -14,8 +14,8 @@ public class CurlWithShema {
                 .baseUri("https://api.dropboxapi.com/")
                 .basePath("/2/file_requests/create \\ ")
                 .contentType(ContentType.JSON)
-               // .body("{\"title\": \"Homework submission\",\"destination\": \"/File Requests/Homework\",\"deadline\": {\"deadline\": \"2020-10-12T17:00:00Z\",\"allow_late_uploads\": \"seven_days\"},\"open\": true}" )
-                .body("\"{ \\n\" +\n" +
+               .body("{\"title\": \"Homework submission\",\"destination\": \"/File Requests/Homework\",\"deadline\": {\"deadline\": \"2020-10-12T17:00:00Z\",\"allow_late_uploads\": \"seven_days\"},\"open\": true}" )
+               /* .body("\"{ \\n\" +\n" +
                         "                        \"\\\"title\\\": \\\"Homework submission\\\", \\n\" +\n" +
                         "                        \"\\\"destination\\\": \\\"/File Requests/Homework\\\", \\n\" +\n" +
                         "                        \"\\\"deadline\\\": { \\n\" +\n" +
@@ -24,7 +24,7 @@ public class CurlWithShema {
                         "                        \"}, \\n\" +\n" +
                         "                        \"\\\"open\\\": true \\n\" +\n" +
                         "                        \"}\\n\" +\n" +
-                        "                        \"\\n\")" )
+                        "                        \"\\n\")" )*/
         .when()
                 .post()
         .then()
@@ -36,3 +36,79 @@ public class CurlWithShema {
 
     }
 }
+//Request method:	POST
+//Request URI:	https://api.dropboxapi.com/2/file_requests/create%20%5C
+//Proxy:			<none>
+//Request params:	<none>
+//Query params:	<none>
+//Form params:	<none>
+//Path params:	<none>
+//Headers:		Accept=*/*
+//				Content-Type=application/json
+//Cookies:		<none>
+//Multiparts:		<none>
+//Body:
+//{
+//    "title": "Homework submission",
+//    "destination": "/File Requests/Homework",
+//    "deadline": {
+//        "deadline": "2020-10-12T17:00:00Z",
+//        "allow_late_uploads": "seven_days"
+//    },
+//    "open": true
+//}
+//HTTP/1.1 307 Temporary Redirect
+//Location: /2/file_requests/create%20/
+//Vary: Accept-Encoding
+//X-Dropbox-Response-Origin: local
+//Date: Thu, 23 Dec 2021 19:14:21 GMT
+//Server: envoy
+//Connection: close
+//Content-Length: 0
+//
+//===============================================
+//Default Suite
+//Total tests run: 1, Passes: 1, Failures: 0, Skips: 0
+//===============================================
+
+
+
+
+//Request method:	POST
+//Request URI:	https://api.dropboxapi.com/2/file_requests/create%20%5C
+//Proxy:			<none>
+//Request params:	<none>
+//Query params:	<none>
+//Form params:	<none>
+//Path params:	<none>
+//Headers:		Accept=*/*
+//				Content-Type=application/json
+//Cookies:		<none>
+//Multiparts:		<none>
+//Body:
+//"{ \n" +
+//                        "\"title\": \"Homework submission\", \n" +
+//                        "\"destination\": \"/File Requests/Homework\", \n" +
+//                        "\"deadline\": { \n" +
+//                        "\"deadline\": \"2020-10-12T17:00:00Z\", \n" +
+//                        "\"allow_late_uploads\": \"seven_days\" \n" +
+//                        "}, \n" +
+//                        "\"open\": true \n" +
+//                        "}\n" +
+//                        "\n")
+//HTTP/1.1 307 Temporary Redirect
+//Location: /2/file_requests/create%20/
+//Vary: Accept-Encoding
+//X-Dropbox-Response-Origin: local
+//Date: Thu, 23 Dec 2021 19:09:01 GMT
+//Server: envoy
+//Connection: close
+//Content-Length: 0
+//
+//===============================================
+//Default Suite
+//Total tests run: 1, Passes: 1, Failures: 0, Skips: 0
+//===============================================
+//
+//
+//Process finished with exit code 0
