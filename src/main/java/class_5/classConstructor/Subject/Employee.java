dc9f33ca3483a0f9cec7dbd1adbe0b8d322a866a
Oleksandr <zahirolek@gmail.com>
2022-06-21 17:49:06 +0300

    Constructor add

--- src/main/java/class_5/classConstructor/Subject/Employee.java
+++ src/main/java/class_5/classConstructor/Subject/Employee.java
@@ -8,10 +8,10 @@ public class Employee {
         age = newAge;
     }
 }
-public class Main {
-    public static void main(String[] args) {
+//public class Main {
+    //public static void main(String[] args) {
         // Employee e =  new Employee();
        // System.out.println(e.name);
-    }
-}
+    //}
+//}
 //В методе main() производится обращение к конструктору без параметров класса Employee. А в классе объявлен конструктор с парамерами. Компилятор не находит конструктора без параметров и выдает ошибку. Ошибки бы не возникло, если бы у класса вообще не было объявлено конструкторов. В этом случае компилятор создал бы конструктор по умолчанию без параметров.
